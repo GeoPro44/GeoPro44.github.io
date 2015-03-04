600bce22fad54488f9ec699862b981eccacd0a1d
$fullname <$email>
2015-03-04 15:41:15 -0500

    fade and stuff

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-var app = angular.module('testApp', []);
+var app = angular.module('testApp', ['ngRoute', 'ngAnimate']);
 
-/*app.config(function ($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
     	.when('/',
 		{
 			controller: 'HomeController',
 			templateUrl: 'templates/home.html'
 		})
-		.when('/page2',
+		.when('/other',
 		{
-			controller: 'Page2Controller',
-			templateUrl: 'templates/page2.html'
+			controller: 'RController',
+			templateUrl: 'templates/other.html'
+		})
+		.when('/pic',
+		{
+			controller: 'HomeController',
+			templateUrl: 'templates/pic.html'
 		});
-    });*/
+    });
 
 /*app.config(function($stateProvider, $urlRouterProvider) {
 	//
@@ -53,29 +58,30 @@ app.controller('HomeController', function ($scope, $document, $timeout) {
 	$scope.name = "Geo";
 	$scope.showMain = true;
 
-	$scope.$on('nav-button-clicked', function(event, args) {
-	    var id = args.page;
-		var someElement = angular.element(document.getElementById(id));
-		$document.scrollToElement(someElement, 50, 1500);
-	});
-	
-	$scope.nav2 = function(page) {
-		if (page == 'other') {
-			$scope.showMain = false;
-			$scope.showPic = false;
-			$scope.showR = true;
-		} else if (page === 'pic') {
-			$scope.showMain = false;
-			$scope.showR = false;
-			$scope.showPic = true;
-		} else {
-			$scope.showMain = true;
-		}
-	}
-
     function init() {
 	
 
+    }
+
+    init();
+
+});
+
+app.controller('RController', function ($scope, $document, $timeout) {
+	
+	$scope.pics = [ 
+		{'imgSrc': 'img/image.jpg', 'name': 'luke' },
+		{'imgSrc': 'img/image2.jpg', 'name': 'new stang' },
+		{'imgSrc': 'img/image3.jpg', 'name': 'old stang' },
+		{'imgSrc': 'img/image4.jpg', 'name': 'trap' },
+		{'imgSrc': 'http://placehold.it/500x300', 'name': 'default' }];
+	
+	$scope.setPic = function(pic) {
+		$scope.mainPic = pic;
+	}
+
+    function init() {
+		$scope.mainPic = $scope.pics[1];
     }
 
     init();
